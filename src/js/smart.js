ef35f9eb2b53d9fe9301c4daabe9c8ffb045da05
Saurabh Dubey <0saurabh@gmail.com>
2016-05-30 21:59:26 +0200

    added web page for app.

---
+++ src/js/smart.js
@@ -0,0 +1,23 @@
+var moods = [1, 2, 3, 34, 45, 66];
+
+
+$(document).ready(function () {
+
+    var div = $('#forecast');
+    var count = 0;
+
+    function changeNews() {
+
+        var forecast = "<i class='icon-" + moods[count++] + " icon'></i><span class='row-text'>Tuesday 31, Thunder Strom</span><span class='row-text'>21 &deg; C</span>";
+
+        div.fadeIn().delay(3500).fadeOut(function () {
+            changeNews();
+        }).html(forecast)
+        if (count == moods.length) {
+            count = 0;
+        }
+    }
+
+    changeNews();
+
+});
