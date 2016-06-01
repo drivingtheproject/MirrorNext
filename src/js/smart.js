024f91aa3da4fdaf1ab89659813daa1101420b4f
Saurabh Dubey <0saurabh@gmail.com>
2016-06-01 03:20:45 +0200

    added time and date update method. and page refresh timeout.

--- src/js/smart.js
+++ src/js/smart.js
@@ -1,23 +1,51 @@
-var moods = [1, 2, 3, 34, 45, 47];
-
+var forecasts = [];
 
 $(document).ready(function () {
 
+    updateDate();
     var div = $('#forecast');
     var count = 0;
+    getWeather();
 
-    function changeForecast() {
+    function rotateForecast() {
 
-        var forecast = "<i class='icon-" + moods[count++] + " iconContent'></i><span class='content-text'>Tuesday 31, Thunder Strom</span><span class='content-text'>21 &deg; C</span>";
-
-        div.fadeIn(600).delay(3500).fadeOut(function () {
-            changeForecast();
-        }).html(forecast)
-        if (count == moods.length) {
+        div.fadeIn(3000).delay(2500).fadeOut(3000, function () {
+            rotateForecast();
+        }).html(forecasts[count++])
+        if (count == forecasts.length) {
             count = 0;
         }
     }
 
-    changeForecast();
-
+    function getWeather() {
+        $.simpleWeather({
+            location: 'Wiesloch',
+            woeid: '',
+            unit: 'c',
+            success: function (weather) {
+                var html = "<i class='icon-" + weather.code + " iconHeader'></i><span class='header-text'>" + weather.temp + "&deg;" + weather.units.temp + "</span>";
+                $("#weather").html(html);
+                for (var i = 0; i < weather.forecast.length; i++) {
+                    forecasts[i] = "<i class='icon-" + weather.forecast[i].code + " iconContent'></i><span class='content-text'>" + weather.forecast[i].day + "," + weather.forecast[i].date + "  |  " + weather.forecast[i].high + "&deg;" + weather.units.temp + ", " + weather.forecast[i].text + "</span>"
+                }
+                rotateForecast();
+                var timestamp = moment(weather.updated.substr(0, weather.updated.length - 5));
+                $("#footer").html('<p>Weather updated ' + moment(timestamp).fromNow() + '&nbsp;&nbsp;</p>');
+            },
+            error: function (error) {
+                $("#weather").html('<p>' + error + '</p>');
+            }
+        });
+    }
+    setInterval(updateTime, 1000);
 });
+
+
+function updateDate() {
+    $("#date").html(moment().format("ddd, MMMM do"));
+}
+
+function updateTime() {
+    $('#time').html(moment().format('HH:mm:ss'));
+
+}
